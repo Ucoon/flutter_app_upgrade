cf844c7035fffa7b4d0a42d121e484e4b90a622c
何宗杰 <zhijian@kooboo.cn>
2022-05-13 17:39:15 +0800

    fix bug

--- ios/Classes/FlutterAppUpgradePlugin.m
+++ ios/Classes/FlutterAppUpgradePlugin.m
@@ -5,7 +5,7 @@
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_app_upgrade-Swift.h"
+#import "kooboo_flutter_app_upgrade-Swift.h"
 #endif
 
 @implementation FlutterAppUpgradePlugin
