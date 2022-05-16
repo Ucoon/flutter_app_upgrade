9a4842b9d1d97e76f49d27a789e71a4710fc3433
何宗杰 <zhijian@kooboo.cn>
2022-05-16 15:53:58 +0800

    fix bug

--- ios/Classes/FlutterAppUpgradePlugin.m
+++ ios/Classes/FlutterAppUpgradePlugin.m
@@ -1,6 +1,6 @@
 #import "FlutterAppUpgradePlugin.h"
-#if __has_include(<flutter_app_upgrade/flutter_app_upgrade-Swift.h>)
-#import <flutter_app_upgrade/flutter_app_upgrade-Swift.h>
+#if __has_include(<kooboo_flutter_app_upgrade/kooboo_flutter_app_upgrade-Swift.h>)
+#import <kooboo_flutter_app_upgrade/kooboo_flutter_app_upgrade-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
